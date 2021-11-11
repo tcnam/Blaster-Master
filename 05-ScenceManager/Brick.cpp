fac5e46aeed738939b17f7eaa7a8a35da35e89ef
NamTran <tcnam2000@gmail.com>
2021-11-11 15:15:06 +0700

    modify brick class for flexible size

--- 05-ScenceManager/Brick.cpp
+++ 05-ScenceManager/Brick.cpp
@@ -1,15 +1,26 @@
 #include "Brick.h"
 
+CBrick::CBrick(float x, float y, int width, int height)
+{
+	this->x = x;
+	this->y = y;
+	this->width = width;
+	this->height = height;
+}
 void CBrick::Render()
 {
-	animation_set->at(0)->Render(x, y);
-	//RenderBoundingBox();
+	//animation_set->at(0)->Render(x, y);
+	RenderBoundingBox();
 }
 
 void CBrick::GetBoundingBox(float &l, float &t, float &r, float &b)
 {
 	l = x;
 	t = y;
-	r = x + BRICK_BBOX_WIDTH;
-	b = y + BRICK_BBOX_HEIGHT;
+	r = x + width;
+	b = y + height;
+}
+CBrick::~CBrick()
+{
+
 }
--- 05-ScenceManager/Brick.h
+++ 05-ScenceManager/Brick.h
@@ -1,12 +1,15 @@
 #pragma once
 #include "GameObject.h"
 
-#define BRICK_BBOX_WIDTH  16
-#define BRICK_BBOX_HEIGHT 16
 
 class CBrick : public CGameObject
 {
+private:
+	int width;
+	int height;
 public:
+	CBrick(float x, float y, int width, int height);
+	~CBrick();
 	virtual void Render();
 	virtual void GetBoundingBox(float &l, float &t, float &r, float &b);
 };
