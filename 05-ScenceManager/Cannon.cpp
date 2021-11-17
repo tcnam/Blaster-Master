8e363dc8592cfccd9d060cdd05ce6ea2f6592da8
NamTran <tcnam2000@gmail.com>
2021-11-17 21:13:14 +0700

    add new object cannon

---
+++ 05-ScenceManager/Cannon.cpp
@@ -0,0 +1,74 @@
+#include <algorithm>
+#include <assert.h>
+#include "Utils.h"
+
+#include "Cannon.h"
+#include "Game.h"
+
+CCannon::CCannon(float x, float y) : CGameObject()
+{
+	SetCannonUP(false);
+	SetState(CANNON_STATE_RIGHT);
+	start_x = x;
+	start_y = y;
+	this->x = x;
+	this->y = y;
+}
+void CCannon::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects)
+{
+
+}
+
+void CCannon::Render()
+{
+	int ani = -1;
+	int alpha = 255;
+	switch (isUp)
+	{
+	case false:
+	{
+		switch (state)
+		{
+		case CANNON_STATE_RIGHT:
+			ani = CANNON_ANI_RIGHT;
+			animation_set->at(ani)->Render(round(x), round(y), alpha);
+			break;
+		case CANNON_STATE_LEFT:
+			ani = CANNON_ANI_LEFT;
+			animation_set->at(ani)->Render(round(x), round(y), alpha);
+			break;
+		}
+		DebugOut(L"[INFO] Tank has been rendered!\n");
+		break;
+	}
+	case true:
+	{
+		switch (state)
+		{
+		case CANNON_STATE_RIGHT:
+			ani = CANNON_ANI_CANNONUP_RIGHT;
+			animation_set->at(ani)->Render(round(x), round(y), alpha);
+			break;
+		case CANNON_STATE_LEFT:
+			ani = CANNON_ANI_CANNONUP_LEFT;
+			animation_set->at(ani)->Render(round(x), round(y), alpha);
+			break;
+		}
+		DebugOut(L"[INFO] Tank has been rendered!\n");
+		break;
+	}
+	}
+
+
+
+}
+
+void CCannon::SetState(int state)
+{
+	CGameObject::SetState(state);
+}
+
+void CCannon::GetBoundingBox(float& left, float& top, float& right, float& bottom)
+{
+
+}
---
+++ 05-ScenceManager/Cannon.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "GameObject.h"
+
+
+#define CANNON_STATE_RIGHT		100
+#define CANNON_STATE_LEFT			200
+
+#define CANNON_ANI_RIGHT					0
+#define CANNON_ANI_LEFT						1
+
+#define CANNON_ANI_CANNONUP_RIGHT			2
+#define CANNON_ANI_CANNONUP_LEFT			3
+
+
+class CCannon : public CGameObject
+{
+
+	bool isUp;
+
+	float start_x;			// initial position of JASON at scene
+	float start_y;
+public:
+	CCannon(float x = 0.0f, float y = 0.0f);
+	virtual void Update(DWORD dt, vector<LPGAMEOBJECT>* colliable_objects = NULL);
+	virtual void Render();
+
+	void SetState(int state);
+	void SetCannonUP(bool UpOrNOt) { isUp = UpOrNOt; }
+
+	virtual void GetBoundingBox(float& left, float& top, float& right, float& bottom);
+};
