4c2af57e28343f547d2d42686babcc926689d55b
NamTran <tcnam2000@gmail.com>
2021-11-24 21:05:27 +0700

    define some constant values

--- 05-ScenceManager/Camera.h
+++ 05-ScenceManager/Camera.h
@@ -4,6 +4,9 @@
 #include "Jason.h"
 
 #define BOUNDRRY_X_MAX 1792.0f
+#define CAMX_IN_USE 330
+#define CAMY_IN_USE 300
+
 
 class Camera
 {
@@ -17,6 +20,8 @@ public:
 	~Camera();
 	void SetJason(CJason* jason) { Jason = jason; }
 	void SetPosition(float x, float y) { this->cam_x = x, this->cam_y = y; }
+	float GetCamX() { return cam_x; }
+	float GetCamY() { return cam_y; }
 	void GetPosition(float& x, float& y) { x = this->cam_x; y = this->cam_y; }
 	void Update(DWORD dt);
 };
