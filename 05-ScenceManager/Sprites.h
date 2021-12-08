1b464d196fc8a3f5e3170f34f90fed01afabf628
NamTran <tcnam2000@gmail.com>
2021-12-08 09:44:50 +0700

    add functions to get left, right, top, bottom of background for calculating background size

--- 05-ScenceManager/Sprites.h
+++ 05-ScenceManager/Sprites.h
@@ -17,7 +17,10 @@ class CSprite
 	LPDIRECT3DTEXTURE9 texture;
 public: 
 	CSprite(int id, int left, int top, int right, int bottom, LPDIRECT3DTEXTURE9 tex);
-
+	int GetLeft() { return left; }
+	int GetTop() { return top; }
+	int GetRight() { return right; }
+	int GetBottom() { return bottom; }
 	void Draw(float x, float y, int alpha = 255);
 };
 
