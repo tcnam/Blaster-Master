f3ea99e695da72b331ae04fdd46c0cbedf5d3d78
NamTran <tcnam2000@gmail.com>
2021-12-28 20:46:50 +0700

    add a function to get render_cam_x and render_cam_y in order to get location for hud to be rendered

--- 05-ScenceManager/Camera.h
+++ 05-ScenceManager/Camera.h
@@ -31,6 +31,7 @@ public:
 	void SetPosition(float x, float y) { this->cam_x = x, this->cam_y = y; }
 	float GetCamX() { return cam_x; }
 	float GetCamY() { return cam_y; }
+	void GetRenderPosition(float& x, float& y) { x = render_cam_x; y = render_cam_y; }
 	void GetPosition(float& x, float& y) { x = this->cam_x; y = this->cam_y; }
 	void Update(DWORD dt);
 };
